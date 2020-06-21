3fa65977de6a460f576ffaa9728d3d4f5b56868d
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-21 20:43:24 +0900

    add init with attr for book model test

--- spec/bookshelf/entities/book_spec.rb
+++ spec/bookshelf/entities/book_spec.rb
@@ -1,3 +1,6 @@
 RSpec.describe Book, type: :entity do
-  # place your tests here
+  it 'can be initialized with attributes' do
+    book = Book.new(title: 'Refactoring')
+    expect(book.title).to eq('Refactoring')
+  end
 end
