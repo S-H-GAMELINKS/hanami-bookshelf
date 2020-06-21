4fb7052b11c346e7484ed11eb5a8a3fb98183486
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-21 20:38:50 +0900

    add list_books_spec.rb

---
+++ spec/web/features/list_books_spec.rb
@@ -0,0 +1,11 @@
+require 'features_helper'
+
+RSpec.describe 'List books' do
+  it 'displays each book on the page' do
+    visit '/books'
+
+    within '#books' do
+      expect(page).to have_css('.book', count: 2)
+    end
+  end
+end
