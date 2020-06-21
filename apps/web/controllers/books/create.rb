3f692af8b652190a0e9083fcf57ff8002d61dfc6
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-21 20:55:53 +0900

    fix books#create action in controller

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -5,6 +5,9 @@ module Web
         include Web::Action
 
         def call(params)
+          BookRepository.new.create(params[:book])
+
+          redirect_to '/books'
         end
       end
     end
