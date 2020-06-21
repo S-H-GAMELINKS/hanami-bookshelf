7501d20788963aad7f518847a4ec7ab03a01f7b9
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-21 20:54:17 +0900

    fix books#create controller test

--- spec/web/controllers/books/create_spec.rb
+++ spec/web/controllers/books/create_spec.rb
@@ -1,9 +1,23 @@
-RSpec.describe Web::Controllers::Books::Create, type: :action do
+RSpec.describe Web::Controllers::Books::Create do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { Hash[book: { title: 'Confident Ruby', author: 'Avdi Grimm' }] }
+  let(:repository) { BookRepository.new }
 
-  it 'is successful' do
+  before do
+    repository.clear
+  end
+
+  it 'creates a new book' do
+    action.call(params)
+    book = repository.last
+
+    expect(book.id).to_not be_nil
+  end
+
+  it 'redirects the user to the books listing' do
     response = action.call(params)
-    expect(response[0]).to eq 200
+
+    expect(response[0]).to eq(302)
+    expect(response[1]['Location']).to eq('/books')
   end
 end
