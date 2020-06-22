41931110d5a02f18526973240782242ecbacabc9
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-06-22 22:57:11 +0900

    validates error test

--- spec/web/features/add_book_spec.rb
+++ spec/web/features/add_book_spec.rb
@@ -1,21 +1,19 @@
 require 'features_helper'
 
 RSpec.describe 'Add a book' do
-  after do
-    BookRepository.new.clear
-  end
+  # Spec written earlier omitted for brevity
 
-  it 'can create a new book' do
+  it 'displays list of errors when params contains errors' do
     visit '/books/new'
 
     within 'form#book-form' do
-      fill_in 'Title',  with: 'New book'
-      fill_in 'Author', with: 'Some author'
-
       click_button 'Create'
     end
 
-    expect(page).to have_current_path('/books')
-    expect(page).to have_content('New book')
+    expect(current_path).to eq('/books')
+
+    expect(page).to have_content('There was a problem with your submission')
+    expect(page).to have_content('Title must be filled')
+    expect(page).to have_content('Author must be filled')
   end
 end
